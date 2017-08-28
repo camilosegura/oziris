0bce865b8f057395fd72d2dc6bdcf3bf0a4259bf
Johan Lizarazo <johanlizarazob@gmail.com>
2017-08-28 00:26:15 -0500

    Add an user accounts

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
  
 import { Tasks } from '../api/tasks.js'; 
  
 import Task from './Task.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
  
 // App component - represents the whole app
 class App extends Component {
@@ -39,6 +41,8 @@ class App extends Component {
       text,
 
       createdAt: new Date(), // current time
+	  owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username,  // username of logged in user
 
     });
 
@@ -87,20 +91,16 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
-		           
-				   <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-
-            <input
-
-              type="text"
-
-              ref="textInput"
-
-              placeholder="Type to add new tasks"
-
-            />
-
-          </form>
+		    <AccountsUIWrapper />       
+			{ this.props.currentUser ?
+				<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+				  <input
+					type="text"
+					ref="textInput"
+					placeholder="Type to add new tasks"
+				  />
+				</form> : ''
+			  }
         </header>
  
         <ul>
@@ -120,5 +120,6 @@ export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
 	incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+	currentUser: Meteor.user(),
   };
 }, App);
