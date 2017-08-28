cb3e5c3a26bf6196848d5808bdfcac7e67e843e0
Andres Ricardo Lizarazo Cardenas <0050.00.29@crieri.local>
2017-08-28 06:58:05 +0200

    is better to create each time a branch

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,20 @@ import Task from './Task.jsx';
  
 // App component - represents the whole app
 class App extends Component {
+	
+	  constructor(props) {
+
+    super(props);
+
+ 
+
+    this.state = {
+
+      hideCompleted: false,
+
+    };
+
+  }
    handleSubmit(event) {
 
     event.preventDefault();
@@ -35,8 +49,26 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
 
   }
+    toggleHideCompleted() {
+
+    this.setState({
+
+      hideCompleted: !this.state.hideCompleted,
+
+    });
+
+  }
+  
   renderTasks() {
-    return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+
+    if (this.state.hideCompleted) {
+
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -45,8 +77,18 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
-		            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+         <h1>Todo List ({this.props.incompleteCount})</h1>
+		  <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+		           
+				   <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
 
             <input
 
@@ -71,10 +113,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
  
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+	incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
