31285505aa8913c56a527e4bf7ca6027d6751f10
Camilo Segura <camsegur1@cobogpgp9531.global.publicisgroupe.net>
2017-09-01 08:18:05 -0500

    reset repo

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className="app-root">
-        <div className="container">
-          {this.props.yield}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app-root">
+                <div className="container">
+                    <AccountsUIWrapper />
+                    {this.props.yield}
+                </div>
+            </div>
+        );
+    }
 }
--- imports/ui/Router.jsx
+++ imports/ui/Router.jsx
@@ -1,44 +1,11 @@
 import React from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import App from './App.jsx';
-import Todo from './Todo.jsx';
-import Route2 from './Route2.jsx';
-import Picture from './Picture.jsx';
-import GPS from './GPS.jsx';
-import Directory from './Directory.jsx';
+import HelloWorld from './HelloWorld.jsx';
 
-
-FlowRouter.route('/Todo', {
-  name: 'Todo',
+FlowRouter.route('/', {
+  name: 'hello-world',
   action() {
-    ReactLayout.render( App, { yield: <Todo /> } );
+    ReactLayout.render( App, { yield: <HelloWorld /> } );
   },
 });
-
-FlowRouter.route('/route-2', {
-    name: 'route-2',
-    action() {
-      ReactLayout.render( App, { yield: <Route2 /> } );
-    },
-  });
-
-  FlowRouter.route('/picture', {
-    name: 'picture',
-    action() {
-      ReactLayout.render( App, { yield: <Picture /> } );
-    },
-  });
-
-  FlowRouter.route('/', {
-    name: 'gps',
-    action() {
-      ReactLayout.render( App, { yield: <GPS /> } );
-    },
-  });
-
-  FlowRouter.route('/directory', {
-    name: 'directory',
-    action() {
-      ReactLayout.render( App, { yield: <Directory /> } );
-    },
-  });
